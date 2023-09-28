0855806ede9f1fcdb901c1bce90ad0aab1677e9e
lindeci <541287039@qq.com>
2023-09-28 16:35:39 +0800

    generate.sh support contos and ubuntu.

--- generate.sh
+++ generate.sh
@@ -1,3 +1,14 @@
-yum install -y flex
+# 检查操作系统类型
+if [ -f /etc/redhat-release ]; then
+    # CentOS 系统
+    sudo yum install -y flex gcc-c++
+elif [ -f /etc/lsb-release ]; then
+    # Ubuntu 系统
+    sudo apt-get install -y flex g++
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
 flex -o sql_fingerprint.cc sql_fingerprint.l 
 g++ sql_fingerprint.cc -o sql_fingerprint
